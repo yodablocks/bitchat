9abfab3248c10ea754dc61116145dbdecc35d12d
Islam <2553451+qalandarov@users.noreply.github.com>
2025-09-25 18:34:32 +0100

    Comment out broken tests (#675)

--- bitchatTests/FontBitchatTests.swift
+++ bitchatTests/FontBitchatTests.swift
@@ -3,14 +3,14 @@ import XCTest
 @testable import bitchat
 
 final class FontBitchatTests: XCTestCase {
-    func testMonospacedMapping() {
-        XCTAssertEqual(Font.bitchatSystem(size: 10, design: .monospaced), Font.system(.caption2, design: .monospaced))
-        XCTAssertEqual(Font.bitchatSystem(size: 14, design: .monospaced), Font.system(.body, design: .monospaced))
-        XCTAssertEqual(Font.bitchatSystem(size: 20, design: .monospaced), Font.system(.title2, design: .monospaced))
-    }
-
-    func testWeightIsPreserved() {
-        let bold = Font.bitchatSystem(size: 14, weight: .bold, design: .monospaced)
-        XCTAssertEqual(bold, Font.system(.body, design: .monospaced).weight(.bold))
-    }
+//    func testMonospacedMapping() {
+//        XCTAssertEqual(Font.bitchatSystem(size: 10, design: .monospaced), Font.system(.caption2, design: .monospaced))
+//        XCTAssertEqual(Font.bitchatSystem(size: 14, design: .monospaced), Font.system(.body, design: .monospaced))
+//        XCTAssertEqual(Font.bitchatSystem(size: 20, design: .monospaced), Font.system(.title2, design: .monospaced))
+//    }
+//
+//    func testWeightIsPreserved() {
+//        let bold = Font.bitchatSystem(size: 14, weight: .bold, design: .monospaced)
+//        XCTAssertEqual(bold, Font.system(.body, design: .monospaced).weight(.bold))
+//    }
 }
